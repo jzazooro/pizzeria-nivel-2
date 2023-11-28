e142d5f8816fb0ef91c229681c29ccf31f372555
jzazooro <91785177+jzazooro@users.noreply.github.com>
2023-11-28 18:36:59 +0100

    falta uml

--- main2.py
+++ main2.py
@@ -1,38 +1,31 @@
 from ejercicio2 import *
 
 def main():
-    documento1 = Documento("Informe1", "Texto", 1024, "Contenido confidencial...")
+    documento1 = Documento("Informe1", "Texto", 3333, "Informacion privilegiada...")
     enlace1 = Enlace("Enlace1", "Enlace", 0)
     carpeta1 = Carpeta("Carpeta1")
     carpeta1.agregar_elemento(documento1)
     carpeta1.agregar_elemento(enlace1)
-
-    print("Elementos de la carpeta:")
+    print("Los elementos de la carpeta son los siguientes:")
     for elemento in carpeta1.elementos:
         print(elemento.nombre)
-
     proxy_acceso = Proxy()
     proxy_acceso.agregar_usuario_autorizado("usuario1")
-
-    print("Intentando acceder al documento con el proxy...")
+    print("Accediendo al documento con el proxy...")
     proxy_acceso.permitir_acceso("usuario1", documento1, "lectura")
-    print("Intentando acceder al documento con el proxy...")
+    print("Accediendo al documento con el proxy...")
     proxy_acceso.permitir_acceso("usuario2", documento1, "lectura")
-
-    print("Intentando acceder a la carpeta con el proxy...")
+    print("Accediendo a la carpeta con el proxy...")
     proxy_acceso.permitir_acceso("usuario1", carpeta1, "lectura")
-    print("Intentando acceder a la carpeta con el proxy...")
+    print("Accediendo a la carpeta con el proxy...")
     proxy_acceso.permitir_acceso("usuario2", carpeta1, "lectura")
-
-    print("Intentando acceder al enlace con el proxy...")
+    print("Accediendo al enlace con el proxy...")
     proxy_acceso.permitir_acceso("usuario1", enlace1, "lectura")
-    print("Intentando acceder al enlace con el proxy...")
+    print("Accediendo al enlace con el proxy...")
     proxy_acceso.permitir_acceso("usuario2", enlace1, "lectura")
-
-    print("Elementos restantes de la carpeta:")
+    print("Los elementos restantes de la carpeta son los siguientes:")
     carpeta1.eliminar_elemento(enlace1)
     for elemento in carpeta1.elementos:
         print(elemento.nombre)
-
 if __name__ == "__main__":
     main()
