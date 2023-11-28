a2b5affa4c448b44fb92be7906b5f0e9f51363a2
jzazooro <91785177+jzazooro@users.noreply.github.com>
2023-11-28 07:41:28 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -4,8 +4,8 @@ def main():
     documento1 = es.Documento("Informe1", "Texto", 1024, "Contenido confidencial...")
     enlace1 = es.Enlace("Enlace1", "Enlace", 0)
     carpeta1 = es.Carpeta("Carpeta1")
-    carpeta1.agregar_elementos(documento1)
-    carpeta1.agregar_elementos(enlace1)
+    carpeta1.agregar_elemento(documento1)
+    carpeta1.agregar_elemento(enlace1)
     print("Elementos de la carpeta: ")
     for i in range(len(carpeta1.elementos)):
         print(carpeta1.elementos[i].nombre)
@@ -16,17 +16,17 @@ def main():
     print("Intentando acceder al documento con el proxy...")
     proxy_acceso.permitir_acceso("usuario1", documento1, "lectura") 
     print("Intentando acceder al enlace con el proxy...")
+    proxy_acceso.permitir_acceso("usuario2", documento1, "lectura")
+
+    print("Intentando acceder a la carpeta con el proxy...")
+    proxy_acceso.permitir_acceso("usuario1", carpeta1, "lectura")
+    print("Intentando acceder a la carpeta con el proxy...")
+    proxy_acceso.permitir_acceso("usuario2", carpeta1, "lectura")
+
+    print("Intentando acceder a la carpeta con el proxy...")
     proxy_acceso.permitir_acceso("usuario1", enlace1, "lectura")
-
     print("Intentando acceder a la carpeta con el proxy...")
-    proxy_acceso.permitir_acceso("usuario1", carpeta1, "lectura")
-    print("Intentando acceder a la carpeta con el proxy...")
-    proxy_acceso.permitir_acceso("usuario1", carpeta1, "escritura")
-
-    print("Intentando acceder a la carpeta con el proxy...")
-    proxy_acceso.permitir_acceso("usuario1", carpeta1, "lectura")
-    print("Intentando acceder a la carpeta con el proxy...")
-    proxy_acceso.permitir_acceso("usuario1", carpeta1, "escritura")
+    proxy_acceso.permitir_acceso("usuario2", enlace1, "lectura")
 
     print("Elementos restantes de la carpeta: ")
     carpeta1.eliminar_elementos(enlace1)
